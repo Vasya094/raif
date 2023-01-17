a64b1e048c843f16322bb7efc7fe938616f61ec5
rinatkhabibulaev <fLukYsat2DXaf9HuRFQK>
2023-01-17 13:15:08 +0200

    try bk image fix

--- theme/indexPage.js
+++ theme/indexPage.js
@@ -1,31 +1,38 @@
-import { makeStyles } from '@material-ui/core/styles';
-import bg from '../public/images/panorama_factory.jpeg';
+import { makeStyles } from "@material-ui/core/styles"
+import bg from "../public/images/panorama_factory.jpeg"
 
-const sectionMargin = (margin) => margin * 15;
+const sectionMargin = (margin) => margin * 15
 const useStyles = makeStyles((theme) => ({
-    mainWrap: {
-      position: 'relative',
-      width: '100%',
-      overflow: 'hidden',
+  mainWrap: {
+    position: "relative",
+    width: "100%",
+    overflow: "hidden",
+  },
+  spaceBottom: {
+    marginBottom: sectionMargin(theme.spacing()),
+  },
+  spaceTop: {
+    paddingTop: theme.spacing(12),
+  },
+  containerWrap: {
+    marginTop: theme.spacing(0),
+  },
+  firstMain: {
+    background: `url(${bg}) no-repeat center center fixed`,
+    backgroundSize: "cover",
+    backgroundColor: "#464646",
+    height: "100vh",
+    "-webkit-background-size": "cover",
+    "-moz-background-size": "cover",
+    "-o-background-size": "cover",
+    "&:after": {
+      background: `url(${bg}) no-repeat center center fixed`,
+      "-webkit-background-size": "cover",
+      "-moz-background-size": "cover",
+      "-o-background-size": "cover",
+      backgroundSize: "cover",
     },
-    spaceBottom: {
-      marginBottom: sectionMargin(theme.spacing()),
-    },
-    spaceTop: {
-      paddingTop: theme.spacing(12),
-    },
-    containerWrap: {
-      marginTop: theme.spacing(0),
-    },
-    firstMain: {
-      backgroundImage: `url(${bg})`,
-      backgroundPosition: 'center center',
-      backgroundRepeat: 'no-repeat',
-      backgroundAttachment: 'fixed',
-      backgroundSize: 'cover',
-      backgroundColor: '#464646',
-      height: '100vh',
-    },
-  }));
+  },
+}))
 
-  export default useStyles;
+export default useStyles
